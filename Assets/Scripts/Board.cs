06eb6be53ecb69cb601bcfebd134bb38cec76bc6
isikoro1 <105261164+isikoro1@users.noreply.github.com>
2023-04-12 21:09:24 +0900

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // やること
+
+    // 変数の作成//
+    //ボード基盤用の四角枠格納用
+    //ボードの高さ
+    //ボードの幅
+    //ボードの高さ調整用数値
+
+    //関数の作成//
+    //ボードを生成する関数の作成
+}
